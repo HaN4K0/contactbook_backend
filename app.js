041b112e0d72ca7f802bf87c26805627de125311
Hanako <minhdang0105682002@gmail.com>
2025-02-26 10:16:08 +0700

    Install for handling errors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -12,5 +14,14 @@ app.use("/api/contacts", contactsRouter);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
+
+//handle 404 response
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+// Code ở đây sẽ chạy khi không tìm thấy route nào,
+// Gọi next để chuyển sang middleware xử lý lỗi
 
 module.exports = app;
